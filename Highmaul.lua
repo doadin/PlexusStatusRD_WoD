45644894cbdb76eb58f43b3443a891b7747e750f
tptbk <tptbk@cac5085b-21ec-44b5-8936-31fddbab59c9>
2014-10-22 03:10:12 +0000

    Update Highmaul Stackable Flags

--- Highmaul.lua
+++ Highmaul.lua
@@ -2,15 +2,15 @@
 local zoneid = 994
 
 --zoneid, debuffID, order, icon_priority, colorpriority, timer, stackable, color, defaultdisable, noicon
---true, true is for stackable
+--, true, true is for stackable
 
 --Trash
 
 --The Butcher
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "The Butcher")
-GridStatusRaidDebuff:DebuffId(zoneid, 156152, 11, 5, 5) --GUSHINGWOUNDS
+GridStatusRaidDebuff:DebuffId(zoneid, 156152, 11, 5, 5, true, true) --GUSHINGWOUNDS
 GridStatusRaidDebuff:DebuffId(zoneid, 156151, 12, 5, 5) --THETENDERIZER
-GridStatusRaidDebuff:DebuffId(zoneid, 156143, 13, 5, 5) --THECLEAVER
+GridStatusRaidDebuff:DebuffId(zoneid, 156143, 13, 5, 5, true, true) --THECLEAVER
 GridStatusRaidDebuff:DebuffId(zoneid, 163046, 14, 5, 5) --PALEVITRIOL
 
 --Kargath Bladefist
@@ -28,10 +28,10 @@ GridStatusRaidDebuff:DebuffId(zoneid, 162497, 29, 5, 5) --ONTHEHUNT
 --Twin Ogron 
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Twin Ogron")
 GridStatusRaidDebuff:DebuffId(zoneid, 158026, 41, 5, 5) --ENFEEBLINGROAR
-GridStatusRaidDebuff:DebuffId(zoneid, 158241, 42, 5, 5) --BLAZE
+GridStatusRaidDebuff:DebuffId(zoneid, 158241, 42, 5, 5, true, true) --BLAZE
 GridStatusRaidDebuff:DebuffId(zoneid, 155569, 43, 5, 5) --INJURED
-GridStatusRaidDebuff:DebuffId(zoneid, 167200, 44, 5, 5) --ARCANEWOUND
-GridStatusRaidDebuff:DebuffId(zoneid, 159709, 45, 5, 5) --WEAKENEDDEFENSES 159709 167179
+GridStatusRaidDebuff:DebuffId(zoneid, 167200, 44, 5, 5, true, true) --ARCANEWOUND
+GridStatusRaidDebuff:DebuffId(zoneid, 159709, 45, 5, 5, true, true) --WEAKENEDDEFENSES 159709 167179
 GridStatusRaidDebuff:DebuffId(zoneid, 163374, 46, 5, 5) --ARCANEVOLATILITY
 
 --Ko'ragh
@@ -49,12 +49,12 @@ GridStatusRaidDebuff:BossNameId(zoneid, 60, "Tectus")
 
 --Brackenspore
 GridStatusRaidDebuff:BossNameId(zoneid, 70, "Brackenspore")
-GridStatusRaidDebuff:DebuffId(zoneid, 163242, 71, 5, 5) --INFESTINGSPORES
+GridStatusRaidDebuff:DebuffId(zoneid, 163242, 71, 5, 5, true, true) --INFESTINGSPORES
 GridStatusRaidDebuff:DebuffId(zoneid, 163590, 72, 5, 5) --CREEPINGMOSS
-GridStatusRaidDebuff:DebuffId(zoneid, 163241, 73, 5, 5) --ROT
+GridStatusRaidDebuff:DebuffId(zoneid, 163241, 73, 5, 5, true, true) --ROT
 GridStatusRaidDebuff:DebuffId(zoneid, 159220, 74, 5, 5) --NECROTICBREATH
 GridStatusRaidDebuff:DebuffId(zoneid, 160179, 75, 5, 5) --MINDFUNGUS
-GridStatusRaidDebuff:DebuffId(zoneid, 159972, 76, 5, 5) --FLESHEATER
+GridStatusRaidDebuff:DebuffId(zoneid, 159972, 76, 5, 5, true, true) --FLESHEATER
 
 --Imperator Mar'gok 
 GridStatusRaidDebuff:BossNameId(zoneid, 80, "Imperator Mar'gok")
@@ -63,4 +63,4 @@ GridStatusRaidDebuff:DebuffId(zoneid, 156467, 82, 5, 5) --DESTRUCTIVERESONANCE  
 GridStatusRaidDebuff:DebuffId(zoneid, 157349, 83, 5, 5) --FORCENOVA  157349 164232 164235 164240
 GridStatusRaidDebuff:DebuffId(zoneid, 158605, 84, 5, 5) --MARKOFCHAOS  158605 164176 164178 164191
 GridStatusRaidDebuff:DebuffId(zoneid, 157763, 85, 5, 5) --FIXATE
-GridStatusRaidDebuff:DebuffId(zoneid, 158553, 86, 5, 5) --CRUSHARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 158553, 86, 5, 5, true, true) --CRUSHARMOR
