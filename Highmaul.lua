a4809815188fd17545c7ab10471136aa3bdb2414
tptbk <tptbk@cac5085b-21ec-44b5-8936-31fddbab59c9>
2014-10-22 03:05:23 +0000

    Acttually Fix Highmaul ZoneID

--- Highmaul.lua
+++ Highmaul.lua
@@ -1,5 +1,5 @@
 --local zone = "Highmaul"
-local zoneid = 944
+local zoneid = 994
 
 --zoneid, debuffID, order, icon_priority, colorpriority, timer, stackable, color, defaultdisable, noicon
 --true, true is for stackable
