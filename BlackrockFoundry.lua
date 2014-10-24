cfdbef8f710ef8383f8c1e3fe4ac7a9ff52953a5
tptbk <tptbk@cac5085b-21ec-44b5-8936-31fddbab59c9>
2014-10-24 14:30:12 +0000

    Update Spell Priorities

--- BlackrockFoundry.lua
+++ BlackrockFoundry.lua
@@ -8,31 +8,30 @@ local zoneid = 988
 
 --Blackhand
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Blackhand")
-GridStatusRaidDebuff:DebuffId(zoneid, 156096, 11, 5, 5) --MARKEDFORDEATH
+GridStatusRaidDebuff:DebuffId(zoneid, 156096, 11, 4, 4) --MARKEDFORDEATH
 GridStatusRaidDebuff:DebuffId(zoneid, 156107, 12, 5, 5) --IMPALED
-GridStatusRaidDebuff:DebuffId(zoneid, 156047, 13, 5, 5) --SLAGGED
+GridStatusRaidDebuff:DebuffId(zoneid, 156047, 13, 6, 6) --SLAGGED
 GridStatusRaidDebuff:DebuffId(zoneid, 156401, 14, 5, 5, true, true) --MOLTENSLAG
-
---BURNED 
-GridStatusRaidDebuff:DebuffId(zoneid, 158054, 15, 5, 5) --SHATTERINGSMASH 158054 155992 159142
+--BURNED 6,6
+GridStatusRaidDebuff:DebuffId(zoneid, 158054, 15, 7, 7) --SHATTERINGSMASH 158054 155992 159142
 GridStatusRaidDebuff:DebuffId(zoneid, 156888, 16, 5, 5) --OVERHEATED
 
 --Beastlord Darmac
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Beastlord Darmac")
-GridStatusRaidDebuff:DebuffId(zoneid, 155365, 21, 5, 5) --PINNEDDOWN
+GridStatusRaidDebuff:DebuffId(zoneid, 155365, 21, 4, 4) --PINNEDDOWN
 GridStatusRaidDebuff:DebuffId(zoneid, 155061, 22, 5, 5) --RENDANDTEAR
-GridStatusRaidDebuff:DebuffId(zoneid, 155030, 23, 5, 5) --SEAREDFLESH
-GridStatusRaidDebuff:DebuffId(zoneid, 155236, 24, 5, 5) --CRUSHARMOR
-GridStatusRaidDebuff:DebuffId(zoneid, 159044, 25, 5, 5) --EPICENTRE
-GridStatusRaidDebuff:DebuffId(zoneid, 162276, 26, 5, 5) --UNSTEADY
-GridStatusRaidDebuff:DebuffId(zoneid, 155657, 27, 5, 5) --FLAMEINFUSION
+GridStatusRaidDebuff:DebuffId(zoneid, 155030, 23, 6, 6) --SEAREDFLESH
+GridStatusRaidDebuff:DebuffId(zoneid, 155236, 24, 6, 6) --CRUSHARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 159044, 25, 6, 6) --EPICENTRE
+GridStatusRaidDebuff:DebuffId(zoneid, 162276, 26, 6, 6) --UNSTEADY
+GridStatusRaidDebuff:DebuffId(zoneid, 155657, 27, 6, 6) --FLAMEINFUSION
 
 --Flamebender Ka'graz
 GridStatusRaidDebuff:BossNameId(zoneid, 30, "Flamebender Ka'graz")
 GridStatusRaidDebuff:DebuffId(zoneid, 155318, 31, 5, 5) --LAVASLASH
-GridStatusRaidDebuff:DebuffId(zoneid, 155277, 32, 5, 5) --BLAZINGRADIANCE
-GridStatusRaidDebuff:DebuffId(zoneid, 154952, 33, 5, 5) --FIXATE
-GridStatusRaidDebuff:DebuffId(zoneid, 155074, 34, 5, 5) --CHARRINGBREATH
+GridStatusRaidDebuff:DebuffId(zoneid, 155277, 32, 4, 4) --BLAZINGRADIANCE
+GridStatusRaidDebuff:DebuffId(zoneid, 154952, 33, 4, 4) --FIXATE
+GridStatusRaidDebuff:DebuffId(zoneid, 155074, 34, 6, 6) --CHARRINGBREATH
 GridStatusRaidDebuff:DebuffId(zoneid, 163284, 35, 5, 5, true, true) --RISINGFLAME
 GridStatusRaidDebuff:DebuffId(zoneid, 162293, 36, 5, 5) --EMPOWEREDARMAMENT
 
@@ -45,18 +44,18 @@ GridStatusRaidDebuff:DebuffId(zoneid, 164380, 44, 5, 5, true, true) --BURNING
 
 --The Blast Furnace
 GridStatusRaidDebuff:BossNameId(zoneid, 50, "The Blast Furnace")
-GridStatusRaidDebuff:DebuffId(zoneid, 155240, 51, 5, 5) --TEMPERED
+GridStatusRaidDebuff:DebuffId(zoneid, 155240, 51, 6, 6) --TEMPERED
 GridStatusRaidDebuff:DebuffId(zoneid, 155242, 52, 5, 5) --HEAT
-GridStatusRaidDebuff:DebuffId(zoneid, 176133, 53, 5, 5) --BOMB
+GridStatusRaidDebuff:DebuffId(zoneid, 176133, 53, 4, 4) --BOMB
 GridStatusRaidDebuff:DebuffId(zoneid, 156934, 54, 5, 5) --RUPTURE
-GridStatusRaidDebuff:DebuffId(zoneid, 175104, 55, 5, 5) --MELTARMOR
-GridStatusRaidDebuff:DebuffId(zoneid, 176121, 56, 5, 5) --VOLATILEFIRE
-GridStatusRaidDebuff:DebuffId(zoneid, 158702, 57, 5, 5) --FIXATE
+GridStatusRaidDebuff:DebuffId(zoneid, 175104, 55, 6, 6) --MELTARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 176121, 56, 4, 4) --VOLATILEFIRE
+GridStatusRaidDebuff:DebuffId(zoneid, 158702, 57, 4, 4) --FIXATE
 GridStatusRaidDebuff:DebuffId(zoneid, 155225, 58, 5, 5) --MELT
 
 --Hans'gar and Franzok 
 GridStatusRaidDebuff:BossNameId(zoneid, 70, "Hans'gar and Franzok")
-GridStatusRaidDebuff:DebuffId(zoneid, 157139, 71, 5, 5) --SHATTEREDVERTEBRAE
+GridStatusRaidDebuff:DebuffId(zoneid, 157139, 71, 6, 6) --SHATTEREDVERTEBRAE
 GridStatusRaidDebuff:DebuffId(zoneid, 161570, 72, 5, 5) --SEARINGPLATES
 GridStatusRaidDebuff:DebuffId(zoneid, 157853, 73, 5, 5) --AFTERSHOCK
 
@@ -64,27 +63,27 @@ GridStatusRaidDebuff:DebuffId(zoneid, 157853, 73, 5, 5) --AFTERSHOCK
 GridStatusRaidDebuff:BossNameId(zoneid, 80, "Gruul")
 GridStatusRaidDebuff:DebuffId(zoneid, 155080, 81, 5, 5) --INFERNOSLICE
 --INFERNOSTRIKE 
-GridStatusRaidDebuff:DebuffId(zoneid, 155078, 82, 5, 5) --OVERWHELMINGBLOWS
-GridStatusRaidDebuff:DebuffId(zoneid, 165300, 83, 5, 5) --FLARE
+GridStatusRaidDebuff:DebuffId(zoneid, 155078, 82, 6, 6) --OVERWHELMINGBLOWS
+GridStatusRaidDebuff:DebuffId(zoneid, 165300, 83, 6, 6) --FLARE
 
 --Kromog
 GridStatusRaidDebuff:BossNameId(zoneid, 90, "Kromog")
 GridStatusRaidDebuff:DebuffId(zoneid, 157060, 91, 5, 5) --RUNEOFGRASPINGEARTH
-GridStatusRaidDebuff:DebuffId(zoneid, 156766, 92, 5, 5) --WARPEDARMOR
-GridStatusRaidDebuff:DebuffId(zoneid, 161839, 93, 5, 5) --RUNEOFCRUSHINGEARTH
+GridStatusRaidDebuff:DebuffId(zoneid, 156766, 92, 6, 6) --WARPEDARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 161839, 93, 7, 7) --RUNEOFCRUSHINGEARTH
 
 --Oregorger
 --GridStatusRaidDebuff:BossNameId(zoneid, 100, "Oregorger")
---GridStatusRaidDebuff:DebuffId(zoneid, ACIDTORRENT, 11, 5, 5)            = 11, -- Oregorger
---GridStatusRaidDebuff:DebuffId(zoneid, RETCHEDBLACKROCK, 11, 5, 5)       = 10, -- Oregorger
---GridStatusRaidDebuff:DebuffId(zoneid, EXPLOSIVESHARD, 11, 5, 5)         = 12, -- Oregorger
---GridStatusRaidDebuff:DebuffId(zoneid, ACIDMAW, 11, 5, 5)                = 10, -- Oregorger
+--GridStatusRaidDebuff:DebuffId(zoneid, ACIDTORRENT, 11, 6, 6)
+--GridStatusRaidDebuff:DebuffId(zoneid, RETCHEDBLACKROCK, 11, 5, 5)
+--GridStatusRaidDebuff:DebuffId(zoneid, EXPLOSIVESHARD, 11, 7, 7)
+--GridStatusRaidDebuff:DebuffId(zoneid, ACIDMAW, 11, 5, 5)
 
 --The Iron Maidens
 GridStatusRaidDebuff:BossNameId(zoneid, 110, "The Iron Maidens")
-GridStatusRaidDebuff:DebuffId(zoneid, 170395, 111, 5, 5) --SORKASPREY
-GridStatusRaidDebuff:DebuffId(zoneid, 170405, 112, 5, 5) --MARAKSBLOODCALLING
-GridStatusRaidDebuff:DebuffId(zoneid, 158692, 113, 5, 5) --DEADLYTHROW
-GridStatusRaidDebuff:DebuffId(zoneid, 158702, 114, 5, 5) --FIXATE
-GridStatusRaidDebuff:DebuffId(zoneid, 158686, 115, 5, 5) --EXPOSEARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 170395, 111, 6, 6) --SORKASPREY
+GridStatusRaidDebuff:DebuffId(zoneid, 170405, 112, 6, 6) --MARAKSBLOODCALLING
+GridStatusRaidDebuff:DebuffId(zoneid, 158692, 113, 6, 6) --DEADLYTHROW
+GridStatusRaidDebuff:DebuffId(zoneid, 158702, 114, 4, 4) --FIXATE
+GridStatusRaidDebuff:DebuffId(zoneid, 158686, 115, 6, 6) --EXPOSEARMOR
 GridStatusRaidDebuff:DebuffId(zoneid, 158683, 116, 5, 5) --CORRUPTEDBLOOD
--- Highmaul.lua
+++ Highmaul.lua
@@ -9,38 +9,38 @@ local zoneid = 994
 --The Butcher
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "The Butcher")
 GridStatusRaidDebuff:DebuffId(zoneid, 156152, 11, 5, 5, true, true) --GUSHINGWOUNDS
-GridStatusRaidDebuff:DebuffId(zoneid, 156151, 12, 5, 5) --THETENDERIZER
+GridStatusRaidDebuff:DebuffId(zoneid, 156151, 12, 6, 6) --THETENDERIZER
 GridStatusRaidDebuff:DebuffId(zoneid, 156143, 13, 5, 5, true, true) --THECLEAVER
 GridStatusRaidDebuff:DebuffId(zoneid, 163046, 14, 5, 5) --PALEVITRIOL
 
 --Kargath Bladefist
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Kargath Bladefist")
 GridStatusRaidDebuff:DebuffId(zoneid, 159113, 21, 5, 5) --IMPALE
-GridStatusRaidDebuff:DebuffId(zoneid, 159178, 22, 5, 5) --OPENWOUNDS
-GridStatusRaidDebuff:DebuffId(zoneid, 159213, 23, 5, 5) --MONSTERSBRAWL
-GridStatusRaidDebuff:DebuffId(zoneid, 158986, 24, 5, 5) --BERSERKERRUSH
+GridStatusRaidDebuff:DebuffId(zoneid, 159178, 22, 6, 6) --OPENWOUNDS
+GridStatusRaidDebuff:DebuffId(zoneid, 159213, 23, 7, 7) --MONSTERSBRAWL
+GridStatusRaidDebuff:DebuffId(zoneid, 158986, 24, 4, 4) --BERSERKERRUSH
 GridStatusRaidDebuff:DebuffId(zoneid, 159410, 25, 5, 5) --MAULINGBREW
-GridStatusRaidDebuff:DebuffId(zoneid, 160521, 26, 5, 5) --VILEBREATH
+GridStatusRaidDebuff:DebuffId(zoneid, 160521, 26, 6, 6) --VILEBREATH
 GridStatusRaidDebuff:DebuffId(zoneid, 159386, 27, 5, 5) --IRONBOMB
 GridStatusRaidDebuff:DebuffId(zoneid, 159188, 28, 5, 5) --GRAPPLE
-GridStatusRaidDebuff:DebuffId(zoneid, 162497, 29, 5, 5) --ONTHEHUNT
+GridStatusRaidDebuff:DebuffId(zoneid, 162497, 29, 4, 4) --ONTHEHUNT
 
 --Twin Ogron 
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Twin Ogron")
-GridStatusRaidDebuff:DebuffId(zoneid, 158026, 41, 5, 5) --ENFEEBLINGROAR
+GridStatusRaidDebuff:DebuffId(zoneid, 158026, 41, 6, 6) --ENFEEBLINGROAR
 GridStatusRaidDebuff:DebuffId(zoneid, 158241, 42, 5, 5, true, true) --BLAZE
 GridStatusRaidDebuff:DebuffId(zoneid, 155569, 43, 5, 5) --INJURED
 GridStatusRaidDebuff:DebuffId(zoneid, 167200, 44, 5, 5, true, true) --ARCANEWOUND
-GridStatusRaidDebuff:DebuffId(zoneid, 159709, 45, 5, 5, true, true) --WEAKENEDDEFENSES 159709 167179
-GridStatusRaidDebuff:DebuffId(zoneid, 163374, 46, 5, 5) --ARCANEVOLATILITY
+GridStatusRaidDebuff:DebuffId(zoneid, 159709, 45, 6, 6, true, true) --WEAKENEDDEFENSES 159709 167179
+GridStatusRaidDebuff:DebuffId(zoneid, 163374, 46, 4, 4) --ARCANEVOLATILITY
 
 --Ko'ragh
 GridStatusRaidDebuff:BossNameId(zoneid, 50, "Ko'ragh")
-GridStatusRaidDebuff:DebuffId(zoneid, 161242, 51, 5, 5) --CAUSTICENERGY
-GridStatusRaidDebuff:DebuffId(zoneid, 162184, 52, 5, 5) --EXPELMAGICSHADOW
-GridStatusRaidDebuff:DebuffId(zoneid, 162186, 53, 5, 5) --EXPELMAGICARCANE
-GridStatusRaidDebuff:DebuffId(zoneid, 163472, 54, 5, 5) --DOMINATINGPOWER
-GridStatusRaidDebuff:DebuffId(zoneid, 162185, 55, 5, 5) --EXPELMAGICFEL
+GridStatusRaidDebuff:DebuffId(zoneid, 161242, 51, 4, 4) --CAUSTICENERGY
+GridStatusRaidDebuff:DebuffId(zoneid, 162184, 52, 6, 6) --EXPELMAGICSHADOW
+GridStatusRaidDebuff:DebuffId(zoneid, 162186, 53, 6, 6) --EXPELMAGICARCANE
+GridStatusRaidDebuff:DebuffId(zoneid, 163472, 54, 4, 4) --DOMINATINGPOWER
+GridStatusRaidDebuff:DebuffId(zoneid, 162185, 55, 7, 7) --EXPELMAGICFEL
 
 --Tectus
 GridStatusRaidDebuff:BossNameId(zoneid, 60, "Tectus")
@@ -52,15 +52,15 @@ GridStatusRaidDebuff:BossNameId(zoneid, 70, "Brackenspore")
 GridStatusRaidDebuff:DebuffId(zoneid, 163242, 71, 5, 5, true, true) --INFESTINGSPORES
 GridStatusRaidDebuff:DebuffId(zoneid, 163590, 72, 5, 5) --CREEPINGMOSS
 GridStatusRaidDebuff:DebuffId(zoneid, 163241, 73, 5, 5, true, true) --ROT
-GridStatusRaidDebuff:DebuffId(zoneid, 159220, 74, 5, 5) --NECROTICBREATH
-GridStatusRaidDebuff:DebuffId(zoneid, 160179, 75, 5, 5) --MINDFUNGUS
-GridStatusRaidDebuff:DebuffId(zoneid, 159972, 76, 5, 5, true, true) --FLESHEATER
+GridStatusRaidDebuff:DebuffId(zoneid, 159220, 74, 4, 4) --NECROTICBREATH
+GridStatusRaidDebuff:DebuffId(zoneid, 160179, 75, 6, 6) --MINDFUNGUS
+GridStatusRaidDebuff:DebuffId(zoneid, 159972, 76, 6, 6, true, true) --FLESHEATER
 
 --Imperator Mar'gok 
 GridStatusRaidDebuff:BossNameId(zoneid, 80, "Imperator Mar'gok")
-GridStatusRaidDebuff:DebuffId(zoneid, 156238, 81, 5, 5) --BRANDED  156238 163990 163989 163988
+GridStatusRaidDebuff:DebuffId(zoneid, 156238, 81, 4, 4) --BRANDED  156238 163990 163989 163988
 GridStatusRaidDebuff:DebuffId(zoneid, 156467, 82, 5, 5) --DESTRUCTIVERESONANCE  156467 164075 164076 164077
 GridStatusRaidDebuff:DebuffId(zoneid, 157349, 83, 5, 5) --FORCENOVA  157349 164232 164235 164240
-GridStatusRaidDebuff:DebuffId(zoneid, 158605, 84, 5, 5) --MARKOFCHAOS  158605 164176 164178 164191
-GridStatusRaidDebuff:DebuffId(zoneid, 157763, 85, 5, 5) --FIXATE
-GridStatusRaidDebuff:DebuffId(zoneid, 158553, 86, 5, 5, true, true) --CRUSHARMOR
+GridStatusRaidDebuff:DebuffId(zoneid, 158605, 84, 4, 4) --MARKOFCHAOS  158605 164176 164178 164191
+GridStatusRaidDebuff:DebuffId(zoneid, 157763, 85, 4, 4) --FIXATE
+GridStatusRaidDebuff:DebuffId(zoneid, 158553, 86, 6, 6, true, true) --CRUSHARMOR
